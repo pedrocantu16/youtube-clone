15a9fb396c6f6f2a7ff9284d8fbb1a5593fb227c
pedro <pedrocantu@gmail.com>
2025-02-15 16:53:09 -0800

    implement web app

---
+++ yt-api-service/functions/src/index.ts
@@ -0,0 +1,21 @@
+import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+import * as logger from "firebase-functions/logger";
+
+initializeApp();
+
+const firestore = new Firestore();
+
+export const createUser = functions.identity.beforeUserCreated(
+  {region: "us-central1"}, (UserRecord) => {
+    const userInfo = {
+      uid: UserRecord.data.uid,
+      email: UserRecord.data.email,
+      photoUrl: UserRecord.data.photoURL,
+    };
+
+    firestore.collection("users").doc(UserRecord.data.uid).set(userInfo);
+    logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+    return;
+  });
